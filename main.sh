67827cbdcf933fe5b341af38a132bd8db15be87c
Tom Dörr <tom.doerr@tum.de>
2021-08-31 13:53:35 +0200

    Update file

--- main.sh
+++ main.sh
@@ -3,18 +3,6 @@
 # Download a random wallpaper image.
 
 # Get the list of all links to images from the front page of the Wallpapers subreddit.
-# wget -qO- https://www.reddit.com/r/wallpapers/top.json?limit=100 | jq -r '.data.children[].data.url' | grep -Eo 'https://i.redd.it/.*' > urls.txt
-
-# Download the image.
-# wget -q "$(head -n 1 urls.txt)" -O /tmp/wallpaper.jpg
-
-# Set the wallpaper.
-# feh --bg-fill /tmp/wallpaper.jpg
-
-# Delete the image.
-# rm /tmp/wallpaper.jpg
-
-# Get the list of all links to images from the front page of the Wallpapers subreddit.
 wget -qO- https://www.reddit.com/r/wallpapers/top.json?limit=100 | jq -r '.data.children[].data.url' | grep -Eo 'https://i.redd.it/.*' > urls.txt
 
 # Select random entry from urls.txt.
