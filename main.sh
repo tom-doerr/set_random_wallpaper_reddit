ddec50b4061fb6fdd858eb8a364af5bb42ec398c
Tom Dörr <tom.doerr@tum.de>
2021-09-02 02:53:22 +0200

    Add symlink for currently used image

--- main.sh
+++ main.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Download a random wallpaper image.
 
 # Get the list of all links to images from the front page of the Wallpapers subreddit.
 wget -qO- https://www.reddit.com/r/wallpapers/top.json?limit=100 | jq -r '.data.children[].data.url' | grep -Eo 'https://i.redd.it/.*' > urls.txt
@@ -20,7 +19,8 @@ mkdir -p ~/Pictures/Wallpapers/i.redd.it/
 echo "$url" >> ~/Pictures/Wallpapers/url.txt
 mv /tmp/wallpaper.jpg ~/Pictures/Wallpapers/
 
-# Rename the image to the url name.
-mv ~/Pictures/Wallpapers/wallpaper.jpg ~/Pictures/Wallpapers/$(echo "$url" | grep -Eo 'i.redd.it/[^/]*')
+target_path=~/Pictures/Wallpapers/$(echo "$url" | grep -Eo 'i.redd.it/[^/]*') 
+mv ~/Pictures/Wallpapers/wallpaper.jpg $target_path
+ln -sf $target_path ~/Pictures/Wallpapers/current.jpg
 rm urls.txt
 
